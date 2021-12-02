1749a61374c05bffac99fb2ae1cf3d79dfae7333
Andrew Johnson <johnsonrobertandrew@gmail.com>
2021-12-02 09:04:11 -0600

    day 2

--- src/days/mod.rs
+++ src/days/mod.rs
@@ -1,2 +1,4 @@
 mod day_1;
+mod day_2;
 pub use day_1::main as one;
+pub use day_2::main as two;
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 mod days;
 
 fn main() {
-    days::one();
+    // days::one();
+    days::two();
 }
